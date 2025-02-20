ad15f96ad157fece486f5ff6e017083881dc2bf3
TinoCapararo <ocapar20@student.scad.edu>
2025-02-20 16:18:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function navigateTo(page) {
-    window.location.href = page;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const feelingBoxes = document.querySelectorAll(".feeling-box");
+
+    feelingBoxes.forEach(box => {
+        box.addEventListener("click", function () {
+            this.style.transform = "rotate(10deg)"; 
+            setTimeout(() => {
+                this.style.transform = "rotate(0deg)"; 
+            }, 300);
+        });
+    });
+});
