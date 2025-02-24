ce2bc425a5401a60eb270e15282ccced761d14f0
JunchaoHeTyler <junche20@student.scad.edu>
2025-02-23 23:33:23 -0500

    update try page

--- script.js
+++ script.js
@@ -10,3 +10,74 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+let emotion = 0; // ranges from 1-4, 0 if unselected
+let place = 0; // ranges from 1-4, 0 if unselected
+
+function handleFeelings(num) {
+    div = document.getElementById('next-btn');
+    card = document.getElementById('e' + num);
+
+    if (emotion == 0) {
+        // cards unselected
+        emotion = num;
+        div.classList.remove('disabled');
+        card.classList.add('selected');
+    } else if (emotion == num) {
+        // card already selected
+        emotion = 0;
+        div.classList.add('disabled');
+        card.classList.remove('selected');
+    } else {
+        // selecting another card
+        document.getElementById('e' + emotion).classList.remove('selected');
+        emotion = num;
+        card.classList.add('selected');
+    }
+    console.log('clicked emotion:' + num);
+}
+
+function handlePlaces(num) {
+    div = document.getElementById('last-button');
+    card = document.getElementById('p' + num);
+
+    if (place == 0) {
+        // cards unselected
+        place = num;
+        div.classList.remove('disabled');
+        card.classList.add('selected');
+    } else if (place == num) {
+        // card already selected
+        place = 0;
+        div.classList.add('disabled');
+        card.classList.remove('selected');
+    } else {
+        // selecting another card
+        document.getElementById('p' + place).classList.remove('selected');
+        place = num;
+        card.classList.add('selected');
+    }
+    console.log('clicked place:' + num);
+}
+
+function handleNext() {
+    if (emotion == 0) {
+        alert('Please select an emotion');
+    } else {
+        console.log("clicked asd")
+        feelings = document.getElementById('feelings');
+        feelings.style.display = 'none';
+        places = document.getElementById('places');
+        places.style.display = 'block';
+    }
+}
+
+function handleFinal() {
+    let result = emotion * place;
+    if (result == 0) {
+        alert('Please select your emotion and place first');
+    } else {
+        console.log(result);
+        window.location.href = "result" + result + ".html";
+    }
+}
